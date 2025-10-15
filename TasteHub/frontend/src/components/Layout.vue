7b7610303f9a16ec4d95b14493258a1e489c21fa
Swirl86 <swesuzzi@gmail.com>
2025-10-15 09:26:55 +0200

    add Layout component for consistent full-height pages

---
+++ TasteHub/frontend/src/components/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div
+        class="relative flex flex-col items-center justify-center text-center px-6 min-h-[calc(100vh-64px)] overflow-hidden"
+    >
+        <slot />
+    </div>
+</template>
